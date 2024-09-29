c6ec72166efefeaae96fddf875e4a0d3e8d8ab2d
kavinkumar36365 <108183218+kavinkumar36365@users.noreply.github.com>
2024-09-29 10:29:34 +0530

    added ham menu in mobile version and made header fixed

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('nav');
+menuBtn.addEventListener("click", () => {
+   menuBtn.classList.toggle('open');
+   navMenu.classList.toggle('open');
+});
